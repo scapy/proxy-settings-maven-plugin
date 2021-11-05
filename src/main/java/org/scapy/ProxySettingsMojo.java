ce3d4899c0abc705ee252d2e605934c66c34924b
Giovanni Scapellato <giovanni.scapellato@gmail.com>
2021-11-06 00:18:57 +0100

    Added javadocs and updated pom

--- src/main/java/org/scapy/ProxySettingsMojo.java
+++ src/main/java/org/scapy/ProxySettingsMojo.java
@@ -7,10 +7,28 @@ import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugins.annotations.Mojo;
 
+/**
+ * This Mojo set the default Authenticator instantiating a PasswordAuthenticator using the system properties 
+ * http.proxyUser and http.proxyPassword.
+ * <br>
+ * If these properties are not set, this Mojo does nothing (it only writes a log).
+ * 
+ * When run the mvn command, add the following parameters: <br>
+ * mvn -Dhttp.proxyUser=myusername -Dhttp.proxyPassword=mypassword -Dhttps.proxyHost=my.proxy.com -Dhttps.proxyPort=443 -Djdk.http.auth.tunneling.disabledSchemes="" ....
+ * 
+ * 
+ * @author Giovanni Scapellato
+ * 
+ * @see java.net.Authenticator
+ * @see java.net.PasswordAuthentication
+ */
 @Mojo(name = "proxysetup")
 public class ProxySettingsMojo extends AbstractMojo {
 
 
+	/**
+	 * Execute method of Mojo plugin 
+	 */
 	public void execute() throws MojoExecutionException
     {
 		final String proxyUser = System.getProperty("http.proxyUser");
